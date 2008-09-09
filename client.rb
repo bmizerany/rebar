39d2bd72b21259e6a42d472b1a5b3b1d24e860ee
bmizerany <blake.mizerany@gmail.com>
2008-09-08 17:21:12 -0700

    More OTP...
      * rebar.erl
        - added rebar:start_link() to use mochiweb_socket_server / gen_server
        - cleaned up formatting
      * client.rb - removed test cruft at bottom that conflicts with use

--- client.rb
+++ client.rb
@@ -9,22 +9,21 @@ module Rebar
       @address = address
       @port = port
     end
-  
+
     def method_missing(*args)
       method, *params = args
       rpc(method, params)
     end
-  
+
     def marshal(fun, args)
       {:method => @mod + ":" + fun.to_s, :params => args, :id => 0}.to_json
     end
-  
+
     def demarshal(str)
       s = JSON.parse(str)
-      p s
       s["result"]
     end
-  
+
     def rpc(fun, args)
       json_request_string = marshal(fun, args)
       json_response_string = nil
@@ -39,32 +38,3 @@ module Rebar
     end
   end
 end
-
-
-# def fac(n)
-#   (2..n).inject(1) { |f, n| f * n }
-# end
-# 
-# erlang = Erlang.new(:funs, '127.0.0.1', 5500)
-# 
-# require 'benchmark'
-# 
-# n = 100
-# Benchmark.bm(7) do |x|
-#   x.report("ruby:") { n.times { fac(50) } }
-#   x.report("erlang:") { n.times { erlang.fac(50) } }
-# end
-
-
-
-
-erlang = Rebar::Erlang.new(:funs, '127.0.0.1', 5500)
-
-ans = erlang.add(1, 2)
-puts "1 + 2 = #{ans}"
-
-ans = erlang.cat("foo", "bar")
-puts "foobar = #{ans}"
-
-ans = erlang.fac(10)
-puts "fac(10) = #{ans}"
